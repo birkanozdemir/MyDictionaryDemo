a7117353369e11ffdfa1f42806fae656558ca295
Birkan Özdemir <birkan_ozdemir@hotmail.com>
2021-01-23 18:32:36 +0300

    Add project files.

---
+++ MyDictionaryDemo/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryDemo
+{
+    class MyDictionary <KeyT, ValueT>
+    {
+        KeyT[] keys;
+        ValueT[] values;
+        public MyDictionary()
+        {
+            keys = new KeyT[0];
+            values = new ValueT[0];
+        }
+        public void Add(KeyT key, ValueT value)
+        {
+            // Temps
+            KeyT[] tempKeys = keys;
+            ValueT[] tempValues = values;
+            // New Arrays
+            keys = new KeyT[keys.Length+1];
+            values = new ValueT[values.Length + 1];
+            // Updated Arrays
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+        public KeyT[] Keys
+        {
+            get { return keys; }
+        }
+        public ValueT[] Values
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ MyDictionaryDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Customer Id, Customer Name
+            MyDictionary<int, string> customers = new MyDictionary<int, string>();
+            customers.Add(1344, "Tolga Demir");
+            customers.Add(1468, "Umut Özkan");
+            customers.Add(455, "Cengiz Çiftçi");
+
+            //customers.GetCustomers();
+
+            for (int i = 0; i < customers.Length; i++)
+            {
+                Console.WriteLine("Müşteri Adı: " + customers.Values[i] + " Id: " + customers.Keys[i]);
+            }
+        }
+    }
+}
